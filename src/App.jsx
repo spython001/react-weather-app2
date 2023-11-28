43aebee62c68d8bab609b37cae7057d4947f2cf9
spython <emmanuelegbo01@gmail.com>
2023-11-28 15:08:27 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 //import { useState } from 'react'
 import './App.scss'
+import Navbar from './navbar/Navbar'
 
 function App() {
   
   return (
     <div className='app'>
       <div className='wrapper'>
-        
+        <Navbar/>
       </div>
     </div>
   )
---
+++ src/navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import './navbar.scss'
+
+export default function Navbar() {
+  return (
+    <div>
+        <input type="text" placeholder="Search" className='content'/>
+        <span><i className="icon content fa-solid fa-magnifying-glass"></i></span>
+    </div>
+  )
+}
